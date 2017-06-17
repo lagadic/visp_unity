f169e2f28f895b611586aff552ac757081dd2f57
Arya Mihir Singh <arya.m.singh.10@gmail.com>
2017-06-17 12:08:34 +0530

    Converting to grey scale

--- vispWrapper/visp-demo.cpp
+++ vispWrapper/visp-demo.cpp
@@ -1,27 +1,32 @@
 #include "visp-demo.h"
 extern "C" {
+	// Defining VpImage globally
 	vpImage<unsigned char> image;
-  double dot_prod(unsigned int* const A){
-    //Defining (1 X 3) Row Vector
-    vpRowVector r(3);
-    r[0] = A[0];
-    r[1] = A[1];
-    r[2] = A[2];
-    //Defining (3 X 1) Coulmn Vector
-    vpColVector c(3);
-    c[0] = A[3];
-    c[1] = A[4];
-    c[2] = A[5];
-    //Dot Product
-    double product = r * c;
-    return product;
-  }
+	unsigned char* bitmap_grey;
 
-  double displayImage(unsigned char* const bitmap, int height, int width) {
-	  double t0 = vpTime::measureTimeMs();
-	  image.resize(height,width);
-	  image.bitmap = bitmap;
-	  vpImageIo::write(image, std::string("C:/Users/mpouliqu/Documents/test.png"));
-	  return vpTime::measureTimeMs() - t0;
-  }
+	double dot_prod(unsigned int* const A){
+		//Defining (1 X 3) Row Vector
+		vpRowVector r(3);
+		r[0] = A[0];
+		r[1] = A[1];
+		r[2] = A[2];
+		//Defining (3 X 1) Coulmn Vector
+		vpColVector c(3);
+		c[0] = A[3];
+		c[1] = A[4];
+		c[2] = A[5];
+		//Dot Product
+		double product = r * c;
+		return product;
+	}
+
+	void passFrame(unsigned char* const bitmap, int height, int width){
+		// Resize frame according to Webcam input from Unity
+		image.resize(height,width);
+		unsigned int size = unsigned(width) * unsigned(height);
+
+		// Convert frame to Grey Scale for the tracker pipeline
+		vpImageConvert::RGBaToGrey(bitmap,bitmap_grey,size);
+		image.bitmap = bitmap_grey;
+	}
 }
