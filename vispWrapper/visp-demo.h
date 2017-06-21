bd939f96aac69bca1f60e8879bcbdc9ae6b0e69e
Arya Mihir Singh <arya.m.singh.10@gmail.com>
2017-06-21 22:53:29 +0530

    include vpConfig.h, modify unity script

--- vispWrapper/visp-demo.h
+++ vispWrapper/visp-demo.h
@@ -1,13 +1,10 @@
-#define TESTDLL_API __declspec(dllexport)
-
-#include <visp/vpMatrix.h>
-#include <visp/vpMath.h>
-#include <visp/vpTranslationVector.h>
-
+#include <visp3/core/vpConfig.h>
+#include <visp3/core/vpMatrix.h>
+#include <visp3/core/vpMath.h>
+#include <visp3/core/vpTranslationVector.h>
 #include <visp3/core/vpImage.h>
-#include <visp3/core/vpTime.h>
 #include <visp3/io/vpImageIo.h>
-#include <visp/vpImageConvert.h>
+#include <visp3/core/vpImageConvert.h>
 
 #include <iostream>
 #include <string>
@@ -15,6 +12,6 @@
 
 using namespace std;
 extern "C" {
-	TESTDLL_API double dot_prod(unsigned int* const A);
-	TESTDLL_API void passFrame(unsigned char* const bitmap, int height, int width);
+	VISP_EXPORT double dot_prod(unsigned int* const A);
+	VISP_EXPORT void passFrame(unsigned char* const bitmap, int height, int width);
 }
