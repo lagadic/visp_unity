09d4f4a0f7b245a4c407fa3df34648561a36d5eb
Arya Mihir Singh <arya.m.singh.10@gmail.com>
2017-06-25 15:50:39 +0530

    changes in cs file

--- unityProject/Assets/demo.cs
+++ unityProject/Assets/demo.cs
@@ -15,12 +15,29 @@ public class demo : MonoBehaviour {
     [DllImport("visp-demo", CallingConvention = CallingConvention.Cdecl, EntryPoint = "passFrame")]
     //Imported function passFrame()
     public static extern void passFrame(byte[] bitmap, int height, int width);
+/*
+    // Import DLL (visp-demo.dll)
+    [DllImport("visp-demo", CallingConvention = CallingConvention.Cdecl, EntryPoint = "initBlobTracker")]
+    //Imported function initBlobTracker()
+    public static extern void initBlobTracker(int blobCenterX, int blobCenterY, bool isClicked, bool init_done);
 
+    // Import DLL (visp-demo.dll)
+    [DllImport("visp-demo", CallingConvention = CallingConvention.Cdecl, EntryPoint = "trackBlob")]
+    //Imported function trackBlob()
+    public static extern void passFrame(byte[] bitmap, int height, int width);
+*/
 
+    // Import DLL (visp-demo.dll)
+    [DllImport("visp-demo", CallingConvention = CallingConvention.Cdecl, EntryPoint = "BlobTracker")]
+    // Imported function BlobTracker()
+    public static extern void BlobTracker(byte[] bitmap, int height, int width, int clickX, int clickY, bool isClicked, bool init_done, double retX, double retY);
 
     public WebCamTexture webcamTexture;
     public Color32[] data;
-
+    public bool isClicked;
+    public bool init_done;
+    public double retX;
+    public double retY;
     //vectors:
     //a = {a1, a2, a3};
     //b = {b1, b2, b3};
@@ -39,13 +56,24 @@ public class demo : MonoBehaviour {
 
         Debug.Log("Dot Product of the vectors is:");
         Debug.Log(dot_prod(vec));
+
+        isClicked = false;
+        init_done = false;
+
+        // Initialize as any values, since values of retX and retY are going to be updated anyway.
+        retX = 0.0;
+        retY = 0.0;
     }
 
     void Update()
     {
-
-        passFrame(Color32ArrayToByteArray(webcamTexture.GetPixels32()), webcamTexture.height, webcamTexture.width);
-
+        // ########################################################################
+        // ### Get the coordinates clickX and clickY from the scene as doubles. ###
+        // ########################################################################
+        //passFrame(Color32ArrayToByteArray(webcamTexture.GetPixels32()), webcamTexture.height, webcamTexture.width);
+        BlobTracker(Color32ArrayToByteArray(webcamTexture.GetPixels32()), webcamTexture.height, webcamTexture.width, clickX, clickY, isClicked, init_done, retX, retY);
+        Debug.Log(retX);
+        Debug.Log(retY);
     }
 
     private static byte[] Color32ArrayToByteArray(Color32[] colors)
